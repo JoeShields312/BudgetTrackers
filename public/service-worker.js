3cbb2643a4533e2f80a0a2dbf671e4a8b1cebf37
Joe Shields <shields.john.joseph@gmail.com>
2020-03-03 22:05:57 -0600

    added db.js to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
+    '/db.js',
   ];
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
